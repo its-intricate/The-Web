cd4911ed62df824faf371bb233b9d793ad24740e
Melissa <mkneale1@hotmail.com>
2020-10-29 15:12:23 +0000

    Attack to reduce HP

--- Battle/lib/player.rb
+++ Battle/lib/player.rb
@@ -1,8 +1,14 @@
 class Player
-  attr_reader :name
+  attr_accessor :name, :hp
 
   def initialize(name)
     @name = name
+    @hp = 100
+  end
+
+  def attack(player)
+    # Melissa to attack and reduce Esther hp by 10
+    player.hp -= 10
   end
 
 end
--- Battle/spec/player_spec.rb
+++ Battle/spec/player_spec.rb
@@ -2,8 +2,16 @@ require 'player'
 
 RSpec.describe Player do
   subject(:melissa) { Player.new('Melissa') }
-  
+  subject(:esther) { Player.new('Esther') }
+
   it 'returns player name' do
     expect(melissa.name).to eq 'Melissa'
+  end
+
+  describe '#attack' do
+    it 'reduces player_2 points by 10' do
+      melissa.attack(esther)
+      expect(esther.hp).to eq 90
+    end
   end
 end
